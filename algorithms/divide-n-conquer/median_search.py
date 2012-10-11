a782fc7dfb6d6b294f73616172214959c911674e
Niklas Andersson <nandersson900@gmail.com>
2012-10-11 16:04:03 +0000

    added some silly comments

--- algorithms/divide-n-conquer/median_search.py
+++ algorithms/divide-n-conquer/median_search.py
@@ -10,12 +10,15 @@ B = [5, 17, 28, 31, 40]
 
 def med(a, b, c):
     C = [a, b, c]
-    C.sort()
+    C.sort()  # O(n log n) worst case
     return C[1]
 
 def median(a, b, n):
-    if n < 1:  return None
-    elif n == 1:  return med(A[a], B[b], B[b+1])
+    # NOTE(nandersson):
+    # * upper bound of O(log n)
+    if n < 1:  return None  # silly
+    elif n == 1:
+        return med(A[a], B[b], B[b+1])  # always O(3*log3) no matter of n
     else:
         k = n/2
         if A[a+k] < B[b+k]:

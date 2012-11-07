94b8cbe9024066210d0a52ea34c26cfd982a775a
Niklas Andersson <nandersson900@gmail.com>
2012-11-07 19:17:28 +0000

    another polish

--- haskell/cardplay/BlackJack.hs
+++ haskell/cardplay/BlackJack.hs
@@ -2,9 +2,6 @@ module BlackJack where
 import Cards
 import Wrapper
 
-hand2 :: Hand
-hand2 = Add (Card (Numeric 2) Hearts)
-            (Add (Card Jack Spades) Empty)
 
 -- Task 3.2:
 -- size hand2
@@ -15,14 +12,6 @@ hand2 = Add (Card (Numeric 2) Hearts)
 -- = 2
 
 
-hand3 :: Hand
-hand3 = Add (Card Queen Spades) Empty
-
-hand4 :: Hand
-hand4 = Add (Card Queen Hearts)
---            (Add (Card King Hearts))
-            (Add (Card (Numeric 2) Hearts) Empty)
-
 -- Task 3.3:
 empty :: Hand
 empty = Empty
@@ -31,21 +20,28 @@ valueRank :: Rank -> Integer
 valueRank Ace = 11
 valueRank (Numeric n) = n
 valueRank _ = 10
+
 valueCard :: Card -> Integer
 valueCard c = valueRank (rank c)
-numberOfAces :: Hand -> Integer
-numberOfAces h = 1
 
-getCard :: Hand -> Card
-getCard (Add card hand) = card
+numberOfAces :: Hand -> Integer
+numberOfAces Empty = 0
+numberOfAces (Add c h)
+        | (rank c) == Ace = 1 + numberOfAces h
+        | otherwise = 0 + numberOfAces h
 
 value :: Hand -> Integer
-value h = 3
+value Empty = 0
+value (Add c h)
+        | ((rank c) == Ace) && (numberOfAces h == 0) = valueCard c + 10 + (value h)
+        | otherwise = (valueCard c) + (value h)
 
 gameOver :: Hand -> Bool
-gameOver h = (value h) >= 21
+gameOver h = (value h) > 21
 
---winner :: Hand -> Hand -> Player
---winner h1 h2 | (value h1) == (valueh2) = 
---             | otherwise = (value h1) > (value h2)
-
+winner :: Hand -> Hand -> Player
+winner h1 h2
+        | gameOver h1 = Bank
+        | gameOver h2 = Guest
+        | (value h1) <= (value h2) = Bank
+        | (value h1) > (value h2) = Guest
